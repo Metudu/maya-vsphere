e1ad0ec65301c0b2e673089af66d9dc89c6b2615
Metudu <tarikucar@gmail.com>
2024-11-26 15:52:59 +0300

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "vsphere_user" {
 
 variable "vsphere_password" {
   type        = string
-  description = "The password for vSphere user"
+  description = "The password for vSphere user. You may want to store this variable in a .auto.tfvars file in order to use it secretly."
   sensitive   = true
 }
 
@@ -16,28 +16,28 @@ variable "vsphere_server" {
 
 variable "datacenter" {
   type        = string
-  description = "The datacenter name"
+  description = "The datacenter name. Currently, using only one datacenter is supported."
 }
 
 variable "resource_pool" {
   type        = set(string)
-  description = "The resource pool name"
+  description = "Set of the resource pool names. You need to import all the resource pools you will use to create the virtual machines."
 }
 
 variable "datastore" {
   type        = set(string)
-  description = "The datastore name"
+  description = "Set of the datastore names. You need to import all the datastores you will use to either create a virtual machine or access the ISO file stored in the datastore."
 }
 
 variable "network" {
   type        = set(string)
-  description = "The network adapter name"
+  description = "Set of the network adapter names. You need to import all the network adapters you will use to create the virtual machines."
 }
 
 variable "templates" {
   type = set(string)
-  description = "Template machine names, if any template will be used to create a virtual machine."
-  default = []
+  description = "Set of the template names, use if any template will be used to create a virtual machine."
+  default = null
 }
 
 variable "machines" {
